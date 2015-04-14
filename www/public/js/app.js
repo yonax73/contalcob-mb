11fe9aa8ab4dd032c77392ce921337966033a7ca
Yonatan Alexis Quintero Rodriguez <yonax73@gmail.com>
2015-04-14 17:00:47 -0500

    working keyboard

---
+++ www/public/js/app.js
@@ -0,0 +1,46 @@
+﻿// Initialize app
+var myApp = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+
+});
+$$(document).on('pageInit', function (e) {
+    var page = e.detail.page;
+    if (page.name === 'employee') {
+        config();
+    }
+});
+try {
+    window.addEventListener('native.keyboardshow', function (e) {
+        var deviceHeight = window.innerHeight;
+        var keyboardHeight = e.keyboardHeight;
+        var height = deviceHeight - keyboardHeight;
+        height = height < 0 ? (height * -1) : height;        
+        document.getElementById('page').style.height = height + 'px';        
+    });
+
+    window.addEventListener('native.keyboardhide', function (e) {       
+        setTimeout(function () {
+            var deviceHeight = window.innerHeight;
+            document.getElementById('page').style.height = deviceHeight + 'px';            
+        },100)
+        
+    });
+
+} catch (ex) {
+    alert(ex.message);
+}
+//init config
+config();
+//config
+function config() {
+    adjusteResizeWindow();
+}
+
+
---
+++ www/public/js/utils.js
@@ -0,0 +1,15 @@
+﻿function adjusteResizeWindow() {
+    var inputs = document.querySelectorAll('input');
+    var n = inputs.length;
+    for (var i = 0; i < n; i++) {
+        var input = inputs[i];
+        input.addEventListener('focus', function (e) {
+            var inp = this;
+            setTimeout(function () {
+                $$('.page-content').scrollTop($$(inp).offset().top);
+                inp.focus();
+            }, 1000);
+
+        }, true);
+    }
+}
